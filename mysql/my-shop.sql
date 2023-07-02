a1d7c7eb971ff1dc7917406bc0be3ff4019a8f51
lesham <leshadavay@gmail.com>
2023-07-02 10:18:12 +0500

    add db sample

--- mysql/my-shop.sql
+++ mysql/my-shop.sql
@@ -84,16 +84,16 @@ CREATE TABLE `orders` (
   CONSTRAINT `fk_orders_order_statuses` FOREIGN KEY (`status`) REFERENCES `order_statuses` (`order_status_id`) ON UPDATE CASCADE,
   CONSTRAINT `fk_orders_shippers` FOREIGN KEY (`shipper_id`) REFERENCES `shippers` (`shipper_id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-INSERT INTO `orders` VALUES (1,6,'2019-01-30',1,NULL,NULL,NULL);
-INSERT INTO `orders` VALUES (2,7,'2018-08-02',2,NULL,'2018-08-03',4);
-INSERT INTO `orders` VALUES (3,8,'2017-12-01',1,NULL,NULL,NULL);
-INSERT INTO `orders` VALUES (4,2,'2017-01-22',1,NULL,NULL,NULL);
-INSERT INTO `orders` VALUES (5,5,'2017-08-25',2,'','2017-08-26',3);
-INSERT INTO `orders` VALUES (6,10,'2018-11-18',1,'Aliquam erat volutpat. In congue.',NULL,NULL);
-INSERT INTO `orders` VALUES (7,2,'2018-09-22',2,NULL,'2018-09-23',4);
-INSERT INTO `orders` VALUES (8,5,'2018-06-08',1,'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis.',NULL,NULL);
-INSERT INTO `orders` VALUES (9,10,'2017-07-05',2,'Nulla mollis molestie lorem. Quisque ut erat.','2017-07-06',1);
-INSERT INTO `orders` VALUES (10,6,'2018-04-22',2,NULL,'2018-04-23',2);
+INSERT INTO `orders` VALUES (1,6,'2023-01-30',1,NULL,NULL,NULL);
+INSERT INTO `orders` VALUES (2,7,'2022-08-02',2,NULL,'2022-08-03',4);
+INSERT INTO `orders` VALUES (3,8,'2021-12-01',1,NULL,NULL,NULL);
+INSERT INTO `orders` VALUES (4,2,'2023-01-22',1,NULL,NULL,NULL);
+INSERT INTO `orders` VALUES (5,5,'2021-08-25',2,'','2021-08-26',3);
+INSERT INTO `orders` VALUES (6,10,'2022-11-18',1,'Aliquam erat volutpat. In congue.',NULL,NULL);
+INSERT INTO `orders` VALUES (7,2,'2021-09-22',2,NULL,'2021-09-23',4);
+INSERT INTO `orders` VALUES (8,5,'2021-06-08',1,'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis.',NULL,NULL);
+INSERT INTO `orders` VALUES (9,10,'2022-07-05',2,'Nulla mollis molestie lorem. Quisque ut erat.','2022-07-06',1);
+INSERT INTO `orders` VALUES (10,6,'2023-04-22',2,NULL,'2023-04-23',2);
 
 
 CREATE TABLE `order_items` (
